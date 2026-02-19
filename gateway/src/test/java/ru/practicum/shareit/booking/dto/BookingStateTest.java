7c8550d1907155a1d82c6699575c4675c483a11e
besporyadokk <rakizWasTaken@yandex.ru>
2026-02-19 23:49:39 +0300

    исправления тестов

--- gateway/src/test/java/ru/practicum/shareit/booking/dto/BookingStateTest.java
+++ gateway/src/test/java/ru/practicum/shareit/booking/dto/BookingStateTest.java
@@ -16,15 +16,3 @@ class BookingStateTest {
         assertThat(BookingState.valueOf("REJECTED")).isEqualTo(BookingState.REJECTED);
     }
 }
-
-class BookingStatusTest {
-
-    @Test
-    void testEnumValues() {
-        assertThat(BookingStatus.values()).hasSize(4);
-        assertThat(BookingStatus.valueOf("WAITING")).isEqualTo(BookingStatus.WAITING);
-        assertThat(BookingStatus.valueOf("APPROVED")).isEqualTo(BookingStatus.APPROVED);
-        assertThat(BookingStatus.valueOf("REJECTED")).isEqualTo(BookingStatus.REJECTED);
-        assertThat(BookingStatus.valueOf("CANCELED")).isEqualTo(BookingStatus.CANCELED);
-    }
-}
